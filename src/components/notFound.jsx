d7469acd24f6a679da0a375b96e09fa516f5fe5c
neha chauhan <nehachauhan79@gmail.com>
2019-06-30 01:19:29 +0530

    Worked on routing

---
+++ src/components/notFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+      return (
+      <div className="nf">
+            Not found !!!
+      </div>
+    );
+  }
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Users from './components/users';
+import Inbox from './components/inbox/inbox';
+import NotFound from './components/notFound';
 import * as serviceWorker from './serviceWorker';
+import {Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom';
+import Navigation from './components/navigation/navigation';
+import Search from './components/search/search';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <div className="App">
+                <Navigation />
+                <Search />
+            </div>
+            <Switch>
+                <Route exact path='/' component={App} />
+                <Route path='/users' component={Users} />
+                <Route path='/inbox' component={Inbox} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
